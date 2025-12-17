03078b35ab99bc20ee0ea87af58eed72c4b3e293
René Bertin <rjvbertin@gmail.com>
2025-12-18 00:59:53 +0100

    Trying to add a sound generator that creates a sound with a spectrum that has the same mean and st.dev. as the input .wav file.
    Here, mean and st.dev. are amplitude-weighted of the log10 of the frequency
    (i.e. a normal distribution in the log10-frequency domain, as the spectrum
    looks more like a normal distribution with a log10 horizontal axis.)
    
    Still doing something wrong somewhere.
    
    Committed from host : Bola

--- music.py
+++ music.py
@@ -9,13 +9,14 @@ import struct
 import sys
 
 # math
-import numpy
-from numpy.fft import fft, fftfreq, rfft, rfftfreq
+import numpy as np
+from numpy.fft import fft, fftfreq, rfft, rfftfreq, irfft
 import math
 
 # documentation
 from api_docs import command, parse_args, help
 
+data_scaled_by_factor = 0
 
 @command
 def get_shannon_rel_entropy(file_name, sample_interval=1, duration=-1):
@@ -25,6 +26,69 @@ def get_shannon_rel_entropy(file_name, sample_interval=1, duration=-1):
     """
     wr = wave.open(file_name, 'r')
 
+    track_fft = get_track_fft(wr, sample_interval=sample_interval, duration=duration)
+
+    print("Calculating Shannon entropy and relative entropy ...", end=' ', flush=True)
+    entropy = _shannon_rel_entropy(track_fft)
+    print(entropy)
+    return entropy[1]
+
+@command
+def generate_average_tone(input_filename, output_filename, duration=-1):
+    """Reads <input_filename>, determines its 1D real-input DFFT
+    (i.e. the usual spectrum), generates a synthetic tone from that
+    spectrum (i.e. the IDFFT) and writes it to <output_filename>
+    which will be a 32-bit, mono wav file of the same duration
+    as the input.
+    DOES NOT WORK (YET)
+    """
+    wr = wave.open(input_filename, 'r')
+    rfft_data = get_track_fft(wr, 1, duration, True)
+    # sneakily take the max excluding the DC component
+    rfft_max = np.max(abs(rfft_data[0][1:]))
+    print("rfft:", np.min(abs(rfft_data[0])), rfft_max, rfft_data[1], rfft_data[2])
+    rfrequencies = rfftfreq(rfft_data[3], 1.0 / wr.getframerate())
+
+    N = len(rfrequencies)
+    bell = np.empty(N, dtype=rfft_data[0].dtype)
+    bell[0] = 0
+    bell[1:N] = gaussian(np.log10(rfrequencies[1:N]), rfft_data[1], rfft_data[2])
+    bell *= rfft_max / np.max(bell)
+    # we don't want to scale the original DC component, so we set that last:
+    bell[0] = rfft_data[0][0]
+    abell = abs(bell)
+    print("bell:", np.min(abell), np.max(abell), bell[int(10**rfft_data[1])], np.average(abell), np.std(abell))
+    abell = []
+
+    # this re-generates the original, mono sound:
+    tone = irfft(rfft_data[0]) / data_scaled_by_factor
+    tone_av = np.average(tone)
+    tone_range = np.max(tone) - np.min(tone)
+    print("original reconstructed sound:", np.min(tone), np.max(tone), tone_range, tone_av, np.std(tone))
+    tone = irfft(bell)
+    t_av = np.average(tone)
+    t_range = np.max(tone) - np.min(tone)
+    tone *= tone_range / t_range
+    print("sound from bell:", np.min(tone), np.max(tone), np.max(tone)-np.min(tone), np.average(tone), np.std(tone))
+    #rfft_data[0].resize(0)
+    ww = wave.open(output_filename, 'w')
+    ww.setnchannels(1)
+    ww.setframerate(wr.getframerate())
+    ww.setnframes(len(tone))
+    ww.setsampwidth(4)
+    ww.writeframes(tone.astype(np.int32))
+
+def gaussian(x, av, stdev):
+    xx = (x - av) / stdev
+    return (
+        1.0 / (np.sqrt(2.0 * np.pi) * stdev) * np.exp(-xx * xx / 2)
+    )
+
+def get_track_fft(wr, sample_interval=1, duration=-1,return_rfft_data=False):
+    """Read the samples from the open wav file, print some
+    simple statistics and calculate/return the full FFT.
+    <sample_interval> is currently ignored.
+    """
     duration=float(duration)
     if duration >= 0:
         print("Reading", duration, "seconds from file, sample interval", sample_interval, "...", end=' ', flush=True)
@@ -44,23 +108,25 @@ def get_shannon_rel_entropy(file_name, sample_interval=1, duration=-1):
     # show some data about the average frequency of the sample.
     # For this, we want the regular/asymmetric spectrum; just calculating it
     # with rfft() turns out to be about as expensive as only taking the already
-    # calculated values for frequencies>0 !
-    #posfreqs = numpy.where(frequencies > 0)
+    # calculated values for frequencies>0 ... so we can provide an option to
+    # return the track's rfft.
     print("\tAverage frequency and std. deviation:", end=' ', flush=True)
     rfrequencies = rfftfreq(len(track), 1.0 / wr.getframerate()) #frequencies[posfreqs]
-    track_rfft = rfft(track) #track_fft[posfreqs]
-    posfreqs = []
-    n = len(track_rfft)
+    track_rfft = rfft(track)
+    N = len(track_rfft)
     # the unweighted average should just be 1/4th of the sampling rate
-    print(numpy.average(rfrequencies), "+-", numpy.std(rfrequencies), "Hz")
-    wavstd = weighted_avg_and_std(rfrequencies, abs(track_rfft))
-    print("\tAmplitude-weighted average frequency and std. deviation:", wavstd[0], "+-", wavstd[1], "Hz")
-    rfrequencies = track_rfft = []
-
-    print("Calculating Shannon entropy and relative entropy ...", end=' ', flush=True)
-    entropy = _shannon_rel_entropy(track_fft)
-    print(entropy)
-    return entropy[1]
+    # exclude rfrequencies[0] because that's not an actual frequency
+    print(np.average(rfrequencies[1:N]), "+-", np.std(rfrequencies[1:N]), "Hz")
+    wavstd = weighted_avg_and_std(np.log10(rfrequencies[1:N]), abs(track_rfft[1:N]))
+    stdev_est = (10**(wavstd[0]+wavstd[1]) - 10**(wavstd[0]-wavstd[1])) / 2.0
+    print("\tAmplitude-weighted log-average frequency and std. deviation:", 10**wavstd[0], "+-", stdev_est, "Hz")
+    rfrequencies = []
+    if return_rfft_data:
+        track_fft = []
+        return (track_rfft, wavstd[0], wavstd[1], len(track))
+    else:
+        track_rfft = []
+        return track_fft
 
 @command
 def plot(domain, file_name, sample_interval=1, duration=-1):
@@ -107,7 +173,7 @@ def _plot_time(file_name, sample_interval=1, duration=-1):
     num_frames = int(maxframes) if maxframes else wr.getnframes()
 
     delta_t = sample_interval / frame_rate
-    t = numpy.arange(0.0, (num_frames - sample_interval) / frame_rate + delta_t, delta_t)
+    t = np.arange(0.0, (num_frames - sample_interval) / frame_rate + delta_t, delta_t)
     
     pyplot(t, track)
 
@@ -136,11 +202,20 @@ def _plot_frequencies(file_name, sample_interval=1, duration=-1):
         maxframes = None
     track = _time_data(wr, sample_interval=sample_interval, max_frames=maxframes)
     frequencies = rfftfreq(len(track), 1 / frame_rate)
+    N = len(frequencies)
 
     #ax = pyplot.axes(xlabel='frequency (Hz)',
                      #ylabel='amplitude (complex modulus)',
                      #title='Amplitudes of frequencies of track {}'.format(file_name))
-    pyplot(frequencies, numpy.abs(rfft(track))/len(frequencies))
+    amps = np.abs(rfft(track))/N
+    pyplot(frequencies, amps)
+
+    wavstd = weighted_avg_and_std(np.log10(frequencies[1:N]), amps[1:N])
+    stdev_est = (10**(wavstd[0]+wavstd[1]) - 10**(wavstd[0]-wavstd[1])) / 2.0
+    print("\tAmplitude-weighted log-average frequency and std. deviation:", 10**wavstd[0], "+-",stdev_est, "Hz")
+    bell = gaussian(np.log10(frequencies[1:N]), wavstd[0], wavstd[1])
+    bell *= np.max(amps) / np.max(bell)
+    pyplot(frequencies[1:N], bell)
 
     xlabel('frequency (Hz)')
     ylabel('amplitude (complex modulus) / N')
@@ -160,6 +235,9 @@ def get_wav_info(file_name):
         num channels
     """
     wr = wave.open(file_name, 'r')
+    return _get_wav_info(wr)
+
+def _get_wav_info(wr):
     sample_width = wr.getsampwidth()
     frame_rate = wr.getframerate()
     num_frames = wr.getnframes()
@@ -185,8 +263,8 @@ def _time_data(wr, sample_interval=1, max_frames=None):
     n_channels = wr.getnchannels()
     ## if n_channels != 2 or sample_width != 2 or frame_rate != 44100:
     if n_channels != 2 or not sample_width in [2,3,4,8]:
-        print("Unsupported sample width or frame rate")
-        get_wav_info(wr)
+        print("Unsupported sample width or frame rate:")
+        print(_get_wav_info(wr))
         raise SystemExit
 
     dim=n_frames
@@ -205,20 +283,20 @@ def _time_data(wr, sample_interval=1, max_frames=None):
         if blockread == 0:
             if sample_width == 8:
                 unpack_format += "ll"
-                chan = numpy.empty([2,dim], long)
+                chan = np.empty([2,dim], long)
             elif sample_width == 4:
                 unpack_format += "ii"
-                chan = numpy.empty([2,dim], int)
+                chan = np.empty([2,dim], int)
             elif sample_width == 2:
                 unpack_format += "hh"
-                chan = numpy.empty([2,dim], numpy.int16)
+                chan = np.empty([2,dim], np.int16)
             else:
-                chan = numpy.empty([2,dim], int)
+                chan = np.empty([2,dim], int)
                 unpack_format += f'{int(sample_width*2)}B'
         dtype = f'<i{sample_width}'
     elif wr.getsampclass() == float:
         if blockread == 0:
-            chan = numpy.empty([2,dim], float)
+            chan = np.empty([2,dim], float)
             unpack_format += f'{int(sample_width/2)}f'
         dtype = f'<f{sample_width}'
 
@@ -228,10 +306,10 @@ def _time_data(wr, sample_interval=1, max_frames=None):
     #HRTime.tic()
     # this probably assumes pcm_sXX samples with the same endianness as the host
     if blockread > 0:
-        # use `numpy.empty` (everywhere) to create arrays because that way any non-initialised
+        # use `np.empty` (everywhere) to create arrays because that way any non-initialised
         # bins should cause a varying output result that should alert the user
         # (we're supposed to allocate and initialise the exact required number of bins!)
-        raw = numpy.empty(dim,dtype=f'S{chunksz}')
+        raw = np.empty(dim,dtype=f'S{chunksz}')
         ii = 0
         for i in range(0,dim,blockread):
             wave_data = wr.readframes(blockread)
@@ -252,10 +330,10 @@ def _time_data(wr, sample_interval=1, max_frames=None):
                 # should never happen, and bumping to 1 will probably raise an error,
                 # but that might be better than letting the situation slide?!
                 splitrange = 1
-            # creating wave_values with numpy.array(wave_data) works and gives an identical
+            # creating wave_values with np.array(wave_data) works and gives an identical
             # array, but the view() method will complain that
             # ValueError: Changing the dtype of a 0d array is only supported if the itemsize is unchanged ...
-            wave_values = numpy.empty(1,dtype=f'S{wave_len}')
+            wave_values = np.empty(1,dtype=f'S{wave_len}')
             wave_values[0] = wave_data
             #try:
             raw[ii:ii+splitrange] = wave_values.view(f'S{chunksz}')
@@ -264,7 +342,7 @@ def _time_data(wr, sample_interval=1, max_frames=None):
             #except:
                 #print(i,ii,splitrange,wave_len)
                 #raise sys.exc_info()[1]
-    # the all-numpy solutions for extracting the channel data from the "raw" array are
+    # the all-np solutions for extracting the channel data from the "raw" array are
     # thanks to 'homer512' (https://stackoverflow.com/a/79847078/1460868)
     if sample_width != 3:
         if blockread == 0:
@@ -277,7 +355,7 @@ def _time_data(wr, sample_interval=1, max_frames=None):
                 chan[1,i] = data[1]
             chan = chan.astype(float)
         else:
-            chan = numpy.frombuffer(raw,dtype=dtype).reshape(dim,2).transpose().astype(float)
+            chan = np.frombuffer(raw,dtype=dtype).reshape(dim,2).transpose().astype(float)
     else:
         if blockread == 0:
             for i in range(0,dim):
@@ -293,41 +371,43 @@ def _time_data(wr, sample_interval=1, max_frames=None):
             rightshift = (bytelength - sample_width) * 8 # for sign extension
             udtype = f'u{bytelength}'
             idtype = f'i{bytelength}'
-            leftshifts = numpy.arange(rightshift, 8 * bytelength, 8, dtype=udtype)
-            chan = ((numpy.frombuffer(raw,dtype=f'<u1').reshape((dim, 2, sample_width)) << leftshifts) \
+            leftshifts = np.arange(rightshift, 8 * bytelength, 8, dtype=udtype)
+            chan = ((np.frombuffer(raw,dtype=f'<u1').reshape((dim, 2, sample_width)) << leftshifts) \
                     .sum(axis=-1, dtype=udtype).astype(idtype) >> rightshift) \
                     .transpose().astype(float)
     raw = []
     # calculate the average of the 2 channels:
-    track = numpy.mean(chan, axis=0)
-    max_sample = numpy.max(abs(track))
+    track = np.mean(chan, axis=0)
+    max_sample = np.max(abs(track))
 
     print() #"Data read in", HRTime.toc(), "s")
 
     time_base = float(sample_interval) / float(frame_rate);
     #chan -= zero
-    max_amp0 = numpy.max(abs(chan[0]))
+    max_amp0 = np.max(abs(chan[0]))
     #if max_amp0 == 0:
-        #print("chan0 range,av,stdev:", numpy.min(chan0), numpy.max(chan0), numpy.mean(chan0), numpy.std(chan0))
-    max_amp1 = numpy.max(abs(chan[1]))
+        #print("chan0 range,av,stdev:", np.min(chan0), np.max(chan0), np.mean(chan0), np.std(chan0))
+    max_amp1 = np.max(abs(chan[1]))
     print("Max abs. channel-0 sample value:", max_amp0)
     print("Max abs. channel-1 sample value:", max_amp1)
     chan[0] /= max_amp0
     chan[1] /= max_amp1
-    min_amp0 = numpy.min(chan[0])
-    max_amp0 = numpy.max(chan[0])
+    min_amp0 = np.min(chan[0])
+    max_amp0 = np.max(chan[0])
     print("Normalised chan0 range: [", min_amp0, "at t=", chan[0].argmin() * time_base, "seconds",
           "] -\n\t[", max_amp0, "at t=", chan[0].argmax() * time_base, "seconds ]")
-    min_amp1 = numpy.min(chan[1])
-    max_amp1 = numpy.max(chan[1])
+    min_amp1 = np.min(chan[1])
+    max_amp1 = np.max(chan[1])
     print("Normalised chan1 range: [", min_amp1, "at t=", chan[1].argmin() * time_base, "seconds",
           "] -\n\t[", max_amp1, "at t=", chan[1].argmax() * time_base, "seconds ]")
 
     #track -= zero;
     print("\nMax abs. channel-mix sample value:", max_sample)
-    track /= max_sample
-    min_sample = numpy.min(track)
-    max_sample = numpy.max(track)
+    global data_scaled_by_factor
+    data_scaled_by_factor = 1.0 / max_sample
+    track *= data_scaled_by_factor
+    min_sample = np.min(track)
+    max_sample = np.max(track)
     print("Normalised range: [", min_sample, "at t=", track.argmin() * time_base, "seconds",
           "] -\n\t[", max_sample, "at t=", track.argmax() * time_base, "seconds ]")
 
@@ -335,7 +415,7 @@ def _time_data(wr, sample_interval=1, max_frames=None):
 
 def _shannon_rel_entropy(track_fft):
     entropy = _entropy(track_fft)
-    return numpy.array([entropy, entropy / math.log(len(track_fft), 2)])
+    return np.array([entropy, entropy / math.log(len(track_fft), 2)])
 
 #import HRTime
 def _entropy(track_fft):
@@ -343,7 +423,7 @@ def _entropy(track_fft):
     #HRTime.tic()
 
     #total_weight = sum([abs(z) for z in track_fft])
-    #total_weight = sum(numpy.abs(track_fft))
+    #total_weight = sum(np.abs(track_fft))
     #track_entropy = 0
     #p_x_sum = 0
     #for z in track_fft:
@@ -351,10 +431,10 @@ def _entropy(track_fft):
         #p_x_sum += p_x
         #track_entropy += p_x * math.log(1/p_x, 2)
 
-    # use numpy's array arithmatic, which is a good 5x faster
-    total_weight = sum(numpy.abs(track_fft))
-    p_x = numpy.abs(track_fft) / total_weight
-    track_entropy = sum( p_x * numpy.log2(1/p_x) )
+    # use np's array arithmatic, which is a good 5x faster
+    total_weight = sum(np.abs(track_fft))
+    p_x = np.abs(track_fft) / total_weight
+    track_entropy = sum( p_x * np.log2(1/p_x) )
 
     #print("entropy calculated in", HRTime.toc(), "s")
     return track_entropy
@@ -369,10 +449,10 @@ def weighted_avg_and_std(values, weights):
 
     values, weights -- NumPy ndarrays with the same shape.
     """
-    average = numpy.average(values, weights=weights)
+    average = np.average(values, weights=weights)
     # Fast and numerically precise:
-    variance = numpy.average((values-average)**2, weights=weights)
-    return (average, math.sqrt(variance))
+    variance = np.average((values-average)**2, weights=weights)
+    return (average, np.sqrt(variance))
 
 if __name__ == '__main__':
     parse_args()
